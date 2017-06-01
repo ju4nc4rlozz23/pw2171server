7a6e8149ba2374b5a39f691fbca3a7c77f9c1cd4
ju4nc4rlozz23 <ju4nc4rloz@hotmail.com>
2017-06-01 14:55:58 -0600

    Implementacion ajax y valida terminado

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -1,8 +1,36 @@
 var iniciaApp = function(){
 	//alert("Hola App");
 	var entrar=function(){
-		alert($("#txtUsuario").val());
-		alert($("#txtClave").val());
+		var usuario=$("#txtUsuario").val();
+		var clave=$("#txtClave").val();
+		//estos parametros se pasaron al php
+		var parametros="opcion=valida"+
+						"&usuario="+usuario+
+						"&clave="+clave+
+						"&id="+Math.random();
+
+		var validaEntrada = $.ajax({
+			method:"POST",
+			url:"php/datos.php", //le solicitamos a esta ruta
+			data:parametros,  //estos datos
+			dataType:"json" //y recibimos un json  
+
+
+		});	
+		//done sustituye al success
+		validaEntrada.done(function(data){
+			// alert(data.respuesta); 
+			if(data.respuesta==true){
+				$("#datosUsuario").hide();
+				$("nav").show("slow");
+			}
+			
+		});
+
+		validaEntrada.fail(function(jqError,textStatus){
+			alert("Solicitud fallida: "+textStatus);
+		});
+
 	}
 	var teclaUsuario=function(tecla){
 		if(tecla.which==13){
--- abc/php/datos.php
+++ abc/php/datos.php
@@ -4,8 +4,8 @@
 	function valida(){
 		$respuesta=false;
 		$conexion=conecta();
-		$u=GetSQLValueString($_GET["usuario"],"text");
-		$c=GetSQLValueString(md5($_GET["clave"]),"text");
+		$u=GetSQLValueString($_POST["usuario"],"text");
+		$c=GetSQLValueString(md5($_POST["clave"]),"text");
 		$consulta=sprintf("select usuario,clave from usuarios where usuario=%s and clave=%s limit 1",$u,$c);
 
 		$resultado=mysql_query($consulta);
@@ -17,10 +17,11 @@
 		print(json_encode($salidaJSON)); //Es un array por que hay que convertirlo, el print de esta manera lo convierte en respuesta
 	}
 
-
+	//Codigo CC
+	//Mi4wMDJ8fDE0OTU1NzA5NDE4NjY7MTQ5NTU3MDk0MTg2NjsxNDk1NjYwMDM0NzA0O2p1NG5jNHJsb3p6fMOnwr3CpAZ8NTMxMzYwLjEwNDE5NTI4Nzk7ODcwMjE2MzYuMTA0MjI1NTU7NjY3Mjs0OzMwMTU5ODIyLjMyMzI2NjYxOzg7MDswOzA7MDswOzA7MDswOzA7NDswOzA7MDswOzA7MDs7MDswOzA7MDswOzA7MDstMTstMTstMTstMTstMTswOzA7MDswOzUwOzA7MDt8NzMsNzMsNDQzMzM3NSwwOzU3LDU4LDY0OTE3NDIsMDs0OCw0OCw5ODc1MzY1LDA7MjcsMjcsMTExMTE5MjUsMDsxOSwxOSwxODAzMjI3NywwOzUsNSw2Njk3NDc3LDA7MCwwLDAsMDswLDAsMCwwOzAsMCwwLDA7MCwwLDAsMDswLDAsMCwwOzAsMCwwLDA7MCwwLDAsMDswLDAsMCwwO3zDp8K/wrDDp8K/wr/Dp8KPwqDDpMKAwoDDpMKDwq/DpsKqwqDDpMKMwoDDpMKAwoDDpMK/woDDpMKAwoDDpMKAwo/DpsKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKowoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDpMKAwoDDocKAwoB8w6fCsMKAw6TCvsKAw6TCu8Knw6XCicKAw6TCgMKEw6TCqsKAw6TCgMKQw6TCgMKAw6TCgMKAw6TCgsKAw6TCgMKAw6TChMKAw6TCsMKAw6TCgMKAw6TCgMKgw6TCgMKAw6TCgMKAw6TChMKAw6TCgMKA%21END%21
 
 	//MENÚ PRINCIPAL
-	$opcion=$_GET["opcion"];
+	$opcion=$_POST["opcion"];
 	switch ($opcion) {
 		case 'valida':
 			valida();
@@ -31,3 +32,6 @@
 			break;
 	}
  ?>
+
+
+
